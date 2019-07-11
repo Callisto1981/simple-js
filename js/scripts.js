82803e98a12da488104331a874de74b5a0b04fbd
Wayne <wabbruscatoorr@gmail.com>
2019-07-10 20:49:13 -0400

    Initial commit

---
+++ js/scripts.js
@@ -0,0 +1,24 @@
+var repository = [Bulbasaur,Charizard,Blastoise]
+
+var bulbasaurHeight = 7;
+var bulbasaurTypes = 'grass';
+var bulbasaur = {
+  name: 'Bulbasaur',
+  height: 7,
+  types: 'grass','poison'
+};
+
+var charizarHeight = 1.7;
+var charizarTypes = 'fire','flying';
+var charizard = {
+  name: 'Charizard',
+  height: 1.7,
+  types: 'fire','flying'
+};
+var blastoiseHeight = 1.6;
+var blastoiseTypes = 'water';
+var blastoise = {
+  name: 'Blastoise',
+  height: 1.6,
+  types: 'water'
+};
