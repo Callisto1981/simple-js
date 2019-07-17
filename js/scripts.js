206856ee77bb6ea5f1be7b7fd2d81acef9ac41fb
Wayne <wabbruscatoorr@gmail.com>
2019-07-17 17:43:34 -0400

    Deleted unneeded "var"s

--- js/scripts.js
+++ js/scripts.js
@@ -1,23 +1,17 @@
 var repository = [bulbasaur, charizard, blastoise];
 
-var bulbasaurHeight = 7;
-var bulbasaurTypes = 'grass';
 var bulbasaur = {
   name: 'Bulbasaur',
   height: 7,
   types: 'grass','poison'
 };
 
-var charizardHeight = 1.7;
-var charizardTypes = 'fire','flying';
 var charizard = {
   name: 'Charizard',
   height: 1.7,
   types: 'fire','flying'
 };
 
-var blastoiseHeight = 1.6;
-var blastoiseTypes = 'water';
 var blastoise = {
   name: 'Blastoise',
   height: 1.6,
