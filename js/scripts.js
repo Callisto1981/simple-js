1f2ac93c67073b4bea7215373ceefbcd37184391
Wayne <wabbruscatoorr@gmail.com>
2019-07-23 12:47:43 -0400

    Changed Name of Paragraph

--- js/scripts.js
+++ js/scripts.js
@@ -1,23 +1,37 @@
 var bulbasaur = {
   name: 'Bulbasaur',
   height: 7,
-  types: 'grass','poison'
+  types:['grass','poison']
 };
 
 var charizard = {
   name: 'Charizard',
   height: 1.7,
-  types: 'fire','flying'
+  types:['fire','flying']
 };
 
 var blastoise = {
   name: 'Blastoise',
   height: 1.6,
-  types: 'water'
+  types:['water']
 };
 
-var repository = [bulbasaur, charizard, blastoise];
+var repository = [bulbasaur,charizard,blastoise];
 
-for (var i = 0; i < repository.length; i++) {
-  document.write(repository[i].name);
+for (var i = 0; i <repository.length; i++) {
+  document.write('<p>'+repository[0].name.+'</p>');
+  document.write('<p>'+repository[0].height+'</p>')
+  break
+}
+
+for (var i = 0; i <repository.length; i++) {
+  document.write('<p>'+repository[1].name+'</p>');
+  document.write('<p>'+repository[1].height+'</p>')
+  break
+}
+
+for (var i = 0; i <repository.length; i++) {
+  document.write('<p>'+repository[2].name+'</p>');
+  document.write('<p>'+repository[2].height+'</p>')
+  break
 }
