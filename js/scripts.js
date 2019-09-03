4013a2fd32f4958e98af557069e5507e79413b55
Wayne <wabbruscatoorr@gmail.com>
2019-09-03 13:56:27 -0400

    Changed quotes to match others in html

--- js/scripts.js
+++ js/scripts.js
@@ -1,22 +1,22 @@
+var bulbasaur = {
+  name: 'Bulbasaur',
+  height: 7,
+  types:['Grass','Poison']
+};
+var charizard = {
+  name: 'Charizard',
+  height: 1.7,
+  types:['Fire','Flying']
+};
+var blastoise = {
+  name: 'Blastoise',
+  height: 1.6,
+  types:['Water']
+};
+}
 var pokemonRepository = (function () {
   var repository = [bulbasaur, charizard, blastoise];
-  var bulbasaur = {
-    name: 'Bulbasaur',
-    height: 7,
-    types:['Grass','Poison']
-  };
-  var charizard = {
-    name: 'Charizard',
-    height: 1.7,
-    types:['Fire','Flying']
-  };
-  var blastoise = {
-    name: 'Blastoise',
-    height: 1.6,
-    types:['Water']
-  };
-}
-
+  
 function add(pokemon) {
   if (
     typeof pokemon === 'object' &&
